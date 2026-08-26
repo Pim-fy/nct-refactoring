a41c8c93349178a2ebcbe9555bedff7e8d2b030e
Pim-fy <climent5373@naver.com>
2026-08-26 14:09:36 +0900

    chore: 기초 개발환경 구성

---
+++ backend/src/main/java/negocut/NegocutApplication.java
@@ -0,0 +1,13 @@
+package negocut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegocutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegocutApplication.class, args);
+	}
+
+}
